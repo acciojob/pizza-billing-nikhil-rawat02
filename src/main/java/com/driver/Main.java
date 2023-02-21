3130290e40f5b669e8884082ccf63b5d59c38c45
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-02-21 17:12:32 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,12 +3,14 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
 
-    Pizza p = new Pizza(true);
-
-
-
-    System.out.println(p.getBill());
-    p.addTakeaway();
+    Pizza p = new Pizza(false);
+    p.addExtraCheese();
     p.addExtraToppings();
+    System.out.println(p.getBill());
+
+    Pizza p1 = new Pizza(true);
+    p1.addTakeaway();
+    p1.addExtraToppings();
+    System.out.println(p1.getBill());
   }
 }
